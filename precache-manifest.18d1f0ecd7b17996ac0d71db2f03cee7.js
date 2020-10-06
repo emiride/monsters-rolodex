0f8902a85695fc3d34de0a34e641cabbc28c0653
emiride <emir.hodzich@gmail.com>
2020-10-06 07:50:39 +0200

    Updates

---
+++ precache-manifest.18d1f0ecd7b17996ac0d71db2f03cee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23fd614a18c4bbb64ce449dd1b3d766c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5dff678bc0993ec12158",
+    "url": "/monsters-rolodex/static/css/main.4c006c30.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dff678bc0993ec12158",
+    "url": "/monsters-rolodex/static/js/main.ce303caa.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
